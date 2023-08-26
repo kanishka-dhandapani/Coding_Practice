3c2706dea80ff558c68d9c26832c200f1e3de395
kanishka-dhandapani <143271630+kanishka-dhandapani@users.noreply.github.com>
2023-08-26 19:48:48 +0530

    Create stringNames.py

---
+++ stringNames.py
@@ -0,0 +1,15 @@
+# METHOD-1
+def fullname(first, second):
+    print("Hello", first, second, "!!!")
+
+first = "ABC"
+second = "CDE"
+fullname(first, second)
+
+# METHOD-2
+def fullname(first, second):
+    print(f'Hello {first} {second}! Heyy!!')
+
+first = "ABC"
+second = "CDE"
+fullname(first, second)
